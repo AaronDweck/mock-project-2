e58d5c92c29e19696f6c6d99a010dc0f95394ef4
Aaron Dweck <aarondweck24@gmail.com>
2025-01-08 15:42:52 +0000

    added some error detection to the put endpoint

--- server.js
+++ server.js
@@ -10,27 +10,39 @@ app.get('/', (req, res) => {
     res.send('Home')
 });
 
+// Get a Pokemon by its name
 app.get('/pokemon/:pokemonName', async (req, res) => {
     const pokemonName = await Pokemon.findOne({'name': req.params.pokemonName})
     
     res.send(pokemonName)
 })
 
+// Add a new pokemon to the DB
 app.post('/pokemon', async (req, res) => {
     const newPokemon = await Pokemon.create(req.body)
     res.status(201).send(newPokemon)
 });
 
+// Delete a Pokemon by its name
 app.delete('/pokemon/:pokemonName', async (req, res) => {
     const pokemonName = await Pokemon.deleteOne({'name': req.params.pokemonName})
     
     res.send(pokemonName)
 });
 
+// Update a Pokemon by its name
 app.put('/pokemon/:pokemonName/:property/:value', async (req, res) => {
     const pokemonName = req.params.pokemonName
     const pokemonProperty = req.params.property
-    const propertyValue = Number(req.params.value)
+    let propertyValue = req.params.value
+    // Error detection for the propertyValue
+    if (!isNaN(propertyValue)){
+        propertyValue = Number(propertyValue)
+    } else if (propertyValue === 'true'){
+        propertyValue = true
+    } else if (propertyValue === 'false'){
+        propertyValue = false
+    }
     const pokemonObj = await Pokemon.updateOne({'name': pokemonName},{[pokemonProperty]: propertyValue})
 
     res.send(pokemonObj)
