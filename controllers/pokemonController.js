d5b3e7083085b18f1ea7489431bf95577b23d87d
Aaron Dweck <aarondweck24@gmail.com>
2025-01-09 20:38:57 +0000

    added some error handling

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -5,39 +5,55 @@ const router = express.Router()
 
 // Add a new pokemon to the DB
 router.route('/pokemon').post(async (req, res) => {
-    const newPokemon = await Pokemon.create(req.body)
-    res.status(201).send(newPokemon)
+    try {
+        const newPokemon = await Pokemon.create(req.body)
+        res.status(201).send(newPokemon)
+    } catch (e) {
+        console.dir(e)
+        if (e.name === 'ValidationError') {
+            res.send({ message: "your request doesn't meet the correct requirements" })
+        } else {
+            res.send({ message: 'There is something wrong with your request' })
+        }
+
+    }
 })
 
 // Get a Pokemon by its name
 router.route('/pokemon/:pokemonName').get(async (req, res) => {
-    const pokemonName = await Pokemon.findOne({'name': req.params.pokemonName})
-    
-    res.send(pokemonName)
+    const pokemonName = await Pokemon.findOne({ 'name': req.params.pokemonName })
+    if (!pokemonName) {
+        res.send({ message: "that is an invalid request" })
+    } else {
+        res.send(pokemonName)
+    }
 })
 
 // Update a Pokemon by its name
 router.route('/pokemon/:pokemonName/:property/:value').put(async (req, res) => {
-    const pokemonName = req.params.pokemonName
-    const pokemonProperty = req.params.property
-    let propertyValue = req.params.value
-    // Error detection for the propertyValue
-    if (!isNaN(propertyValue)){
-        propertyValue = Number(propertyValue)
-    } else if (propertyValue === 'true'){
-        propertyValue = true
-    } else if (propertyValue === 'false'){
-        propertyValue = false
-    }
-    const pokemonObj = await Pokemon.updateOne({'name': pokemonName},{[pokemonProperty]: propertyValue})
+    try {
+        const pokemonName = req.params.pokemonName
+        const pokemonProperty = req.params.property
+        let propertyValue = req.params.value
+
+        const pokemonObj = await Pokemon.updateOne({ 'name': pokemonName }, {[pokemonProperty]: propertyValue}, {runValidators: true})
     
-    res.send(pokemonObj)
+        res.send(pokemonObj)
+    } catch (e){
+        if (e.name === 'CastError'){
+            res.send({ message: "your value doesn't meet the correct requirements" })
+        } else if(e.name === 'ValidationError') {
+            res.send({ message: "your request doesn't meet the correct requirements" })
+        } else {
+            console.log(e)
+        }
+    }
 })
 
 // Delete a Pokemon by its name
 router.route('/pokemon/:pokemonName').delete(async (req, res) => {
-    const pokemonName = await Pokemon.deleteOne({'name': req.params.pokemonName})
-    
+    const pokemonName = await Pokemon.deleteOne({ 'name': req.params.pokemonName })
+
     res.send(pokemonName)
 })
 
--- models/pokemon.js
+++ models/pokemon.js
@@ -1,11 +1,36 @@
 import mongoose from 'mongoose'
 
 const pokemonSchema = new mongoose.Schema({
-    number: {type: Number, required: true},
-    name: {type: String, required: true},
-    type: {type: String, required: true},
-    hp: {type: Number, required: true},
-    starter: {type: Boolean, required: true},
+    number: {
+        type: Number, 
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    type: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function (value) {
+                console.log('test:',value)
+                // Ensure the string does not represent a number
+                return isNaN(value);
+            },
+            message: (props) => `${props.value} is not a valid type. It cannot be a number.`
+        }
+        
+    },
+    hp: {
+        type: Number,
+        required: true
+    },
+    starter: {
+        type: Boolean,
+        required: true
+    },
 })
 
 export default mongoose.model('Pokemon', pokemonSchema)
